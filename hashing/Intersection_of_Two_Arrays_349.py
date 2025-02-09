c6563d188c32cb3818230c087d41471b2d5f9e3c
SCUTZKX0621 <1993944596@qq.com>
2025-02-09 19:06:31 +0800

    添加了一个set交集的方法

--- hashing/Intersection_of_Two_Arrays_349.py
+++ hashing/Intersection_of_Two_Arrays_349.py
@@ -7,7 +7,7 @@
 
 class Solution:
     @staticmethod
-    def intersection(self, nums1: list[int], nums2: list[int]) -> list[int]:
+    def intersection1(self, nums1: list[int], nums2: list[int]) -> list[int]:
         if not nums1 or not nums2:
             return []
         list1 = set(nums1)
@@ -18,6 +18,6 @@ class Solution:
                 list1.remove(num)
         return result
 
-    def test(self):
-        print(349)
-
+    def intersection2(self, nums1: list[int], nums2: list[int]) -> list[int]:
+        # python集合可以直接实现交集的运算
+        return list(set(nums1) & set(nums2))
